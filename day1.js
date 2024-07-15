861295f7fc99b3870a27c8e552cae32e93f4c23b
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-07-15 20:21:28 +0530

    Day 1 of js challenge

---
+++ day1.js
@@ -0,0 +1,37 @@
+// //Task 1
+// var num = 5;
+// console.log(num);
+
+// //Task 2
+// let str = "string";
+// console.log(str);
+
+// //Task 3
+// const bool = true;
+// console.log(bool);
+
+// //Task 4
+// const num = 1;
+// const str = "string";
+// const bool = true;
+// const arr = [1, 2, 3, 4, 5];
+// const arr1 = Array(arr);
+// const obj = { a: 1, b: 2, c: 3, d: 4 };
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof arr1);//All arrays are objects in  JS.
+// console.log(typeof obj);
+
+// // Task 5
+// let var1 = 5;
+// console.log(var1);
+// var1 = 10;
+// console.log(var1);
+
+// // Task 6
+// const var1 = 5;
+// var1 = 10;
+// console.log(var1); //TypeError: Assignment to constant variable.
+
