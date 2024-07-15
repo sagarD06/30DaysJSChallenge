91b03396990aebc954e4d8cf189aedceb20b5b07
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-07-15 20:21:48 +0530

    Day 2 of JS challenege

---
+++ day2.js
@@ -0,0 +1,41 @@
+//Activity 1
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Sum = ", num1 + num2);
+console.log("Difference = ", num1 - num2);
+console.log("Product = ", num1 * num2);
+console.log("Divident = ", num1 / num2);
+console.log("Remainder = ", num1 % num2);
+
+//Activity 2
+
+let var1 = 10;
+var1 += 10;
+console.log(var1);
+var1 -= 5;
+console.log(var1);
+
+//Activity 3
+
+let nums1 = 10;
+let nums2 = 5;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+num2 = "10";
+console.log(num1 == num2);
+console.log(num1 === num2); // Strict comparision checks value and its data type.
+
+//Activity 4
+let a = 1;
+let b = 2;
+console.log(a > 2 && b); // Prints false since 1st condition is not true.
+console.log(a > 2 || b);
+console.log(a != b);
+
+//Activity 5
+let number = 1;
+console.log(number > 0 ? "positive" : "negative");
