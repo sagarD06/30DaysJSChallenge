7b551800b6b1d48caebd8b01140633879ac0806b
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-07-17 20:03:17 +0530

    day4 of JS challenge

---
+++ day4.js
@@ -0,0 +1,76 @@
+// Activity 1
+
+//--Task1--
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+//--Task2--
+
+// for(let i=1; i<=10; i++){
+//     console.log(i*5);
+// }
+
+// Activity 2
+
+//--Task3--
+// let i = 1;
+// let sum = 0;
+// while (i <= 10) {
+//   sum += i;
+//   i++;
+// }
+// console.log(sum);
+
+//--Task4--
+// let i = 10;
+// while(i > 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// Activity 3
+
+//--Task5--
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// --Task6--
+// let num = 5;
+// let i = 1;
+// let fact = 1;
+// do {
+//   fact *= i;
+//   i++;
+// } while (i <= num);
+
+// console.log(fact)
+
+// Activity 4
+
+// -- Task7--
+// for (let i = 0; i <= 5; i++) {
+//   let pattern = "";
+//   for (let j = 0; j < i; j++) {
+//     pattern += "* ";
+//   }
+//   console.log(pattern);
+// }
+
+// Activity 5
+
+//--Task8--
+for(let i=1;i<=10;i++) {
+    if(i === 5) continue;
+    console.log(i)
+}
+
+//--Task9--
+
+for(let i = 1 ; i<=10 ;i++){
+    if(i === 7) break;
+    console.log(i)
+}
